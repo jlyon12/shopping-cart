ec6f257e638e937f1766d85ebef58b646aedb7fd
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-06 20:10:48 -0600

    update Home page copy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -19,7 +19,8 @@ const Home = () => {
 				<img src={HeroImg} alt="Clothes on a rack" />
 			</div>
 			<section className={styles.featuredProducts}>
-				<h2>Basics Collection &apos;23</h2>
+				<p className={styles.smallText}>Latest Collection</p>
+				<h2>Basics &apos;23</h2>
 				<div className={styles.productGrid}>{renderCards}</div>
 			</section>
 		</main>
